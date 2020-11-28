f8db4cd39b3d89dcaa5e656bbc81a6a6b73102c3
Mubarak Ibrahim <elmubarak333@gmail.com>
2020-11-28 22:34:15 +0100

    progress

--- lib/helpers/file_convert_helper.dart
+++ lib/helpers/file_convert_helper.dart
@@ -13,11 +13,12 @@ Future displayDialog(BuildContext context, String title, String text) =>
       ),
     );
 
-class FileConvertHelper {
-  List<int> bytes = [];
-  int recieved = 0;
-  int total = 0;
+File pickedFile;
+List<int> bytes = [];
+int recieved = 0;
+int total = 0;
 
+class FileConvertHelper {
   Future uploadDocument(
       {String filePath,
       String fileName,
@@ -29,47 +30,50 @@ class FileConvertHelper {
         await http.MultipartFile.fromPath('file', filePath),
       );
       var res = await request.send();
+      //works at once
       total = res.contentLength;
-      print('start size before sink $recieved');
+      print('start size before write $recieved');
       if (res.contentLength == 0) {
         return res.contentLength;
+      }
+
+      String getDir = await ExtStorage.getExternalStorageDirectory();
+      String folderToSaveConver = 'Narr/Converted';
+      String fileToSave = fileName;
+      Directory fullFolderDirToSave = Directory('$getDir/$folderToSaveConver');
+
+      if (await fullFolderDirToSave.exists()) {
+        res.stream.listen(
+          (value) async {
+            bytes.addAll(value);
+            recieved += value.length;
+          },
+        ).onDone(() async {
+          final fileToWrite = File(
+            '${fullFolderDirToSave.path}/${fileToSave.split('.')[0]}.pdf',
+          );
+          await fileToWrite.writeAsBytes(bytes);
+          print('start size after write  $recieved');
+          print('total size $total');
+          pickedFile = fileToWrite;
+          if (recieved == total) {
+            print('ok');
+            // bytes.clear();
+            recieved = 0;
+          } else if (recieved < total) {
+            print('less bytes');
+          } else {
+            print('too much');
+          }
+        });
+
+        displayDialog(
+            context, "Success", "$fileName file converted successfully");
+        print('file path => ${pickedFile.path}');
+        print(
+            'status code ::==> ${res.statusCode} and reason phrase ::==> ${res.reasonPhrase}');
       } else {
-        String getDir = await ExtStorage.getExternalStorageDirectory();
-        String folderToSaveConver = 'Narr/Converted';
-        String fileToSave = fileName;
-        Directory fullFolderDirToSave =
-            Directory('$getDir/$folderToSaveConver');
-
-        if (await fullFolderDirToSave.exists()) {
-          File file = File(
-              '${fullFolderDirToSave.path}/${fileToSave.split('.')[0]}.pdf');
-
-          // var sink = file.openWrite();
-          // await fileStream.pipe(sink);
-          // sink.close();
-          res.stream.listen(
-            (value) {
-              bytes.addAll(value);
-              recieved += value.length;
-            },
-          ).onDone(() async {
-            final file = File(
-              '${fullFolderDirToSave.path}/${fileToSave.split('.')[0]}.pdf',
-            );
-            await file.writeAsBytes(bytes);
-            print('start size during sink  $recieved');
-          });
-          displayDialog(
-              context, "Success", "$fileName file converted successfully");
-          print('file path => ${file.path}');
-          print('start size after sink $recieved');
-
-          print(
-              'status code ::==> ${res.statusCode} and reason phrase ::==> ${res.reasonPhrase}');
-        } else {
-          await fullFolderDirToSave.create(recursive: true);
-        }
-        return res.contentLength;
+        await fullFolderDirToSave.create(recursive: true);
       }
     } catch (e) {
       print(e);
