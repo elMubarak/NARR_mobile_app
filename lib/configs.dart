0d2891346137c4af8425480ab60a799439bde4bd
musjib999 <musjib999@gmail.com>
2021-06-01 17:07:14 +0100

    test on local server

--- lib/configs.dart
+++ lib/configs.dart
@@ -1,6 +1,6 @@
 const baseUrl = 'https://api.narr.ng';
 const serverUrl = 'https://api.narr.ng/api/v1';
-const mockServerUrl = 'http://192.168.88.42:3000/api/v1';
+const mockServerUrl = 'http://192.168.42.197:3000/api/v1';
 
 const socketServerUrl = 'wss://api.narr.ng';
 const mockSocketServerUrl = 'http://192.168.88.42:3000';
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -412,7 +412,7 @@ class _RegisterState extends State<Register> {
                                 });
 
                                 NetworkHelper(
-                                  url: myUrl,
+                                  url: '$mockServerUrl/auth/register',
                                 )
                                     .userRegistration(
                                   username: email,
@@ -423,8 +423,6 @@ class _RegisterState extends State<Register> {
                                   phone: phone,
                                   address: address,
                                   institution: institutionObject['institution'],
-                                  // _dropdownHelper.selectedInstitutionType,
-                                  // _dropdownHelper.selectedInstitutionName,
                                   context: context,
                                 )
                                     .then((value) {
